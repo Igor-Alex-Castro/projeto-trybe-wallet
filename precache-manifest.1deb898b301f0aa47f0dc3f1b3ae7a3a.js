67ccf4eb582c979780caf2a3398b57808ea7eed1
igor alex <igoralex8701@gmail.com>
2022-06-02 13:51:42 -0300

    Updates

---
+++ precache-manifest.1deb898b301f0aa47f0dc3f1b3ae7a3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bbfabea5ad0619dc88e18c776839bf",
+    "url": "/projeto-trybe-wallet/index.html"
+  },
+  {
+    "revision": "c2a818ea0482e8ffedfc",
+    "url": "/projeto-trybe-wallet/static/css/main.ba8a7ac6.chunk.css"
+  },
+  {
+    "revision": "bd83069ac1113037057e",
+    "url": "/projeto-trybe-wallet/static/js/2.c6c8a89c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projeto-trybe-wallet/static/js/2.c6c8a89c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a818ea0482e8ffedfc",
+    "url": "/projeto-trybe-wallet/static/js/main.6d0eb51f.chunk.js"
+  },
+  {
+    "revision": "cbc79fdf58e946221758",
+    "url": "/projeto-trybe-wallet/static/js/runtime-main.d0caf032.js"
+  },
+  {
+    "revision": "c50bc96956dc53404bca4baf71ab80d6",
+    "url": "/projeto-trybe-wallet/static/media/editar.c50bc969.png"
+  },
+  {
+    "revision": "837c7aa47227f88ee37020ccb184243b",
+    "url": "/projeto-trybe-wallet/static/media/excluir.837c7aa4.png"
+  },
+  {
+    "revision": "eed662317bc82aae4892c1faf8ee4079",
+    "url": "/projeto-trybe-wallet/static/media/login.eed66231.jpg"
+  }
+]);
